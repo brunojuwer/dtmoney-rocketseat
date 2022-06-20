ad4caee80cb6eeac2012f0cbb9f7eb03f53d036d
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-06-20 14:30:44 -0300

    Installed MirageJS and Axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
+import { api } from '../../services/api';
+
 
 export function TransactionTable() {
+
+    useEffect(() => {
+        api.get('/transactions')
+          .then(response => console.log(response.data))
+    }, []);
+
+
+
     return (
         <Container>
             <table>
